dffe5f908e893e3326ef07001af1f74580141cac
Jonathan Bourdett <jab2279@columbia.edu>
2011-12-24 09:05:17 -0500

    read in word list. hash them into a hashset

--- src/Breathalyzer.java
+++ src/Breathalyzer.java
@@ -1,4 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Breathalyzer {
+	
+	private HashSet<String> dictHash;
+	private String theWordListFilename;
+	private static final boolean DEBUG = true;
+	
+	public Breathalyzer(String theWordListFilename) throws Exception{
+		this.theWordListFilename = theWordListFilename;
+		File theWordList = new File(theWordListFilename);
+		try {
+			Scanner dictionary = new Scanner(theWordList);
+			createDictHashSet(dictionary);
+			
+			if (DEBUG)
+				printDictHash();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+	}
 
+	private void createDictHashSet(Scanner s) {
+		dictHash = new HashSet<String>();
+		while(s.hasNextLine()){
+			dictHash.add(s.nextLine().trim());
+		}
+	}
+	
+	private void printDictHash(){
+		System.out.println("Dictionary according to " + theWordListFilename + ":");
+		System.out.println("----------------------------------------------------");
+		
+		Iterator<String> dictHashIterator = dictHash.iterator();
+		while (dictHashIterator.hasNext()){
+			System.out.println(dictHashIterator.next());
+		}
+		
+		
+	}
 }
--- src/BreathalyzerTest.java
+++ src/BreathalyzerTest.java
@@ -3,9 +3,14 @@ public class BreathalyzerTest {
 
 	/**
 	 * @param args
+	 * @throws Exception 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+//		try {
+			/*Breathalyzer myBreathalyzer = */new Breathalyzer(args[0]);
+//		} catch (Exception e) {
+//			System.out.println("Something is wrong with program.");
+//		}
 
 	}
 
