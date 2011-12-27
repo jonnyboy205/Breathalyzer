5054a4b1dcaa8f539408b639acdd6dbde9ffabe2
Jonathan Bourdett <jab2279@columbia.edu>
2011-12-27 03:41:38 -0500

    added functionality for all successors. need to test

--- src/BFS.java
+++ src/BFS.java
@@ -41,9 +41,63 @@ public class BFS {
 	private ArrayList<Node> generateSuccessors(Node currentNode){
 		ArrayList<Node> successors = new ArrayList<Node>();
 		
+		String copyWord = currentNode.getWord();
+	
+		//remove any single letter
+		char[] letters = copyWord.toCharArray();
+		String temp = copyWord;
+		for (int i=0; i<letters.length; i++){
+			temp = copyWord;
+			if (i==0)
+				temp = temp.substring(i+1, letters.length);			
+			else if (i>=1 || i<(letters.length-1))
+				temp = temp.substring(0, i) + temp.substring(i+1, letters.length);
+			else // i==letters.length-1
+				temp = temp.substring(0, i);
+			successors.add(new Node(temp, currentNode));
+		}
+		
+		//replacing any single letter
 		char letter = 'a';
+		for (int i=0; i<letters.length; i++){
+			temp = copyWord;
+			if (i==0){
+				letter = 'a';
+				while (letter <= 'z'){
+					temp = temp.substring(i+1, letters.length);
+					String letterStr = String.valueOf(letter);
+					temp = letterStr.concat(temp);
+					successors.add(new Node(temp, currentNode));
+					letter++;
+				}
+			}
+			else if (i>=1 || i<(letters.length-1)){
+				letter = 'a';
+				while (letter <= 'z'){
+					String letterStr = String.valueOf(letter);
+					if (!letterStr.equals(temp.substring(i, i+1))){
+						temp = temp.substring(0, i) + letterStr + temp.substring(i+1, letters.length);
+						successors.add(new Node(temp, currentNode));
+					}
+					letter++;
+				}
+			}
+			else{ // i==letters.length-1
+				letter = 'a';
+				while (letter <= 'z'){
+					String letterStr = String.valueOf(letter);
+					if (!letterStr.equals(temp.substring(i, i+1))){
+						temp = temp.substring(0, i) + letterStr;
+						successors.add(new Node(temp, currentNode));
+					}
+					letter++;
+				}				
+			}
+		}
+		
+		//adding a letter in any position
 		for (int i=-1; i<currentNode.getWord().length(); i++){
-			if (i==-1){
+			if (i == -1){
 				letter = 'a';
 				while (letter <= 'z'){
 					successors.add(new Node(letter + currentNode.getWord().substring(i+1, currentNode.getWord().length()), currentNode));
